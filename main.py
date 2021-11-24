b0d41c46a57b9946af19e8603c4d56f21bcafc30
cosmic|duck <68925893+cosmicducks@users.noreply.github.com>
2021-11-24 00:22:05 +0000

    more features

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 #emitswoh
 from discord.ext import commands, tasks
 import discord
@@ -106,7 +105,190 @@ async def on_message(message:discord.Message):
 
 
 
+def nitrounchecked(type):
+	if type == "nitro classic":
+		length = 16
+	elif type == "nitro":
+		length = 24
+	code = list(string.ascii_letters + string.digits)
+	random.shuffle(code)
+	code = random.choices(code, k=length)
+	random.shuffle(code)
+	code = "".join(code)
+	return code
 
+
+@bot.event
+async def on_message(message):
+
+	if message.content.startswith("!h"):
+		he = discord.Embed(title="Help", color=0xf47fff)
+		he.add_field(name="!g (nitro/nitro classic)",
+		             value="Generate 1 unchecked nitro/nitro classic code")
+		he.add_field(name="!gc (nitro/nitro classic)",
+		             value="Generate and check 1 nitro/nitro classic code")
+		he.add_field(name="!c (nitro code/nitro classic code)",
+		             value="Check a nitro/nitro classic code")
+		he.add_field(
+		    name="!pc (nitro code/nitro classic code)",
+		    value=
+		    "Searches for proxys for nitro codes. 1 in a 100 chance of finding a valid code."
+		)
+		he.add_field(name="More commands soon",
+		             value="Specials commands for VIP!!")
+		he.add_field(name="!h", value="Help")
+
+		await message.channel.send(embed=he)
+
+	if message.content.startswith(
+	    "*sh") and message.channel.id == vip_commands:
+		txt = discord.Embed(title="Special")
+		txt.discord.Embed(title="Test", value="test")
+		await message.channel.send(embed=he)
+
+	if message.content.startswith("!pc") and message.channel.id == comch:
+		await message.channel.send("Searching......")
+		await asyncio.sleep(5)
+		nitro_codes = [
+		    'https://discord.com/gifts/7YhkBXb7taf5QM9XpNPk73UU',
+		    'https://discord.com/gifts/6eyPp6m7NAfZ6dYn8TEZuRPG'
+		]
+
+		number = random.randint(1, 100)
+		if number == 52:
+
+			await message.channel.send("Proxy found, printing code.")
+			await asyncio.sleep(1)
+			await message.channel.send((random.choice(nitro_codes)))
+			await asyncio.sleep(0.5)
+			await message.channel.send(
+			    "Error Code: 404 Status. Try checking your internet, or try the command again."
+			)
+		else:
+
+			await message.channel.send("No proxies found. Try again in 1 hour")
+
+	if (message.content.startswith("!untilTrue")):
+		ntype = message.content[4:]
+		ntype.strip()
+		ntype = ntype.lower()
+		await message.channel.send("Searching for: " + ntype.split(" ")[1])
+		if ntype == "nitro" or ntype == "nitro classic":
+			code = nitrounchecked(ntype)
+
+			url = "https://discordapp.com/api/v8/entitlements/gift-codes/" + code
+			response = requests.get(url)
+
+			while response.status_code != 200:
+				code = nitrounchecked(ntype)
+
+				url = "https://discordapp.com/api/v8/entitlements/gift-codes/" + code
+				response = requests.get(url)
+			x = "Valid"
+			channel = bot.get_channel(dropch)
+			await channel.send("https://discord.gift/" + code)
+
+			if len(code) == 16:
+				t = "Nitro Classic"
+			elif len(code) == 24:
+				t = "Nitro"
+
+			emb = discord.Embed(title=aemo + " Discord Nitro " + aemo,
+			                    color=0xf47fff)
+
+			emb.add_field(name="Code", value=code, inline=False)
+			emb.add_field(name="Type", value=t, inline=True)
+			emb.add_field(name="Status", value=x, inline=True)
+
+			await message.channel.send(message.author.mention +
+			                           " Check your DMs for " + ntype + "!")
+			await message.author.send(embed=emb)
+			await message.author.send("https://discord.gift/" + code)
+	if message.content.startswith("!g") and message.channel.id == comch:
+		ntype = message.content[3:]
+		ntype.strip()
+		ntype = ntype.lower()
+		if ntype == "nitro" or ntype == "nitro classic":
+			code = nitrounchecked(ntype)
+			await message.author.send("https://discord.gift/" + code)
+			await message.channel.send(message.author.mention +
+			                           " Check your DMs for " + ntype + "!")
+
+	if message.content.startswith("!c") and message.channel.id == comch:
+		code = message.content[3:]
+		code.strip()
+		if len(code) == 16 or len(code) == 24:
+
+			url = "https://discordapp.com/api/v8/entitlements/gift-codes/" + code
+			response = requests.get(url)
+
+			if response.status_code == 200:
+				x = "Valid"
+				channel = bot.get_channel(dropch)
+				await channel.send("https://discord.gift/" + code)
+
+			elif response.status_code == 429:
+				x = "Ratelimited"
+			elif response.status_code == 404:
+				x = "Expired"
+			else:
+				x = "Invalid"
+
+			if len(code) == 16:
+				t = "Nitro Classic"
+			elif len(code) == 24:
+				t = "Nitro"
+			else:
+				t = "Invalid"
+
+			emb1 = discord.Embed(title=emo + " Discord Nitro " + emo,
+			                     color=0xf47fff)
+
+			emb1.add_field(name="Code", value=code, inline=False)
+			emb1.add_field(name="Type", value=t, inline=True)
+			emb1.add_field(name="Status", value=x, inline=True)
+
+			await message.channel.send(embed=emb1)
+
+	if message.content.startswith("!gc") and message.channel.id == comch:
+
+		ntype = message.content[4:]
+		ntype.strip()
+		ntype = ntype.lower()
+		if ntype == "nitro" or ntype == "nitro classic":
+			code = nitrounchecked(ntype)
+
+			url = "https://discordapp.com/api/v8/entitlements/gift-codes/" + code
+			response = requests.get(url)
+
+			if response.status_code == 200:
+				x = "Valid"
+				channel = bot.get_channel(dropch)
+				await channel.send("https://discord.gift/" + code)
+
+			elif response.status_code == 429:
+				x = "Ratelimited"
+			elif response.status_code == 404:
+				x = "Expired"
+			else:
+				x = "Invalid"
+
+			if len(code) == 16:
+				t = "Nitro Classic"
+			elif len(code) == 24:
+				t = "Nitro"
+
+			emb = discord.Embed(title=aemo + " Discord Nitro " + aemo,
+			                    color=0xf47fff)
+
+			emb.add_field(name="Code", value=code, inline=False)
+			emb.add_field(name="Type", value=t, inline=True)
+			emb.add_field(name="Status", value=x, inline=True)
+
+			await message.channel.send(message.author.mention +
+			                           " Check your DMs for " + ntype + "!")
+			await message.author.send(embed=emb)
+			await message.author.send("https://discord.gift/" + code)
 
 
 
